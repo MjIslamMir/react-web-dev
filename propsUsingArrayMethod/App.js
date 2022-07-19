dd2938520f94536499b6547c6d73616d76d45d3d
Mujahi Dul Islam <51232949+MjIslamMir@users.noreply.github.com>
2022-07-19 13:58:04 +0530

    Update App.js

--- propsUsingArrayMethod/App.js
+++ propsUsingArrayMethod/App.js
@@ -1,3 +1,9 @@
+const Courses = (props) => {
+    console.log(props);
+    const {name,exercises}=props;
+    return ( <div>Name of the Course is :: {name} and number of Exercises are :: {exercises}</div> );
+}
+
 const App=() =>{ <div>Name of the Course is </div>
     const course = 'Full Stack application development';
     const parts = [
@@ -22,6 +28,7 @@ const App=() =>{ <div>Name of the Course is </div>
             <p>Hello from the other Side</p>
             
             <h1>{course}</h1>
+            <Courses name={parts[0].name} exercises={parts[0].exercises}/>
             {
             parts.map((val)=>
             <h5 key={val.name}> Name of the course is : {val.name} and number of exercises are : {val.exercises}</h5>)
