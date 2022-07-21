f397a5eb2370af760840c95c804d5f4fded4ec7f
Mujahi Dul Islam <51232949+MjIslamMir@users.noreply.github.com>
2022-07-21 12:53:40 +0530

    Create counterApp.js

---
+++ setState/counterApp.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './App.css'
+//Class based Componet are stateful components
+//Counter App
+class App extends React.Component{
+  state={
+    val:0
+  }
+  incrementCounter=()=>{
+    this.setState({
+      val:this.state.val+1
+    })
+  }
+  decrementCounter=()=>{
+    this.setState({
+      val:this.state.val-1
+    })
+  }
+  reset=()=>{
+    this.setState({
+      val:0
+    })
+  }
+  render(){
+    return (<div>
+    <h1>Counter Value :: {this.state.val}</h1>
+    <button onClick={this.incrementCounter}>increment Counter</button><br/>
+    <button onClick={this.decrementCounter}>Decrement Counter</button><br/>
+    <button onClick={this.reset}>Reset Counter</button>
+    </div> )
+
+  }
+}
+export default App;
